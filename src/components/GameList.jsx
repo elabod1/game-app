d74521cbfabaa0ec740f4828bec7708e773de326
Mohamed Kharaba <msk_h85@hotmail.com>
2025-01-20 20:29:21 +0300

    Show loading Skeletons

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,8 +1,10 @@
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameList = () => {
-    const { games, error } = useGames();
+    const { games, error, isLoading } = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
         <>
@@ -12,6 +14,7 @@ const GameList = () => {
                 </div>
             )}
             <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+                {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
                 {games.map((game) => (
                     <GameCard key={game.id} game={game} />
                 ))}
