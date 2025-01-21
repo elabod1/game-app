cc3f8aaf5be536082b10a40374b57ce4827992ae
Mohamed Kharaba <msk_h85@hotmail.com>
2025-01-21 20:49:24 +0300

    Add structure Platform Filter

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,14 @@
 import GameList from "./GameList";
+import PlatformSelector from "./PlatformSelector";
 
 const MainContent = ({ selectGenre }) => {
     return (
         <>
             <div className="main-content col-span-5">
                 <h1>Games</h1>
-                <div className="filter">Filter</div>
+                <div className="filter my-3">
+                    <PlatformSelector />
+                </div>
                 <div className="cover-card">
                     <GameList selectGenre={selectGenre} />
                 </div>
