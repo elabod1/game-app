547282a3778300004012cc92aa1232d007e6e97b
Mohamed Kharaba <msk_h85@hotmail.com>
2025-01-28 20:31:01 +0300

    Create dynamic Heading

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,13 @@
 import GameList from "./GameList";
 import PlatformSelector from "./PlatformSelector";
 import SortSelector from "./SortSelector";
+import { GameHeading } from "./GameHeading";
 
 const MainContent = ({ selectGenre, onSelectPlatform, selectPlatform, selectSortOrder, onSelectSortOrder, searchText }) => {
     return (
         <>
             <div className="main-content col-span-5">
-                <h1>Games</h1>
+                <GameHeading selectGenre={selectGenre} selectPlatform={selectPlatform} />
                 <div className="filter my-3">
                     <PlatformSelector onSelectPlatform={onSelectPlatform} selectPlatform={selectPlatform} />
                     <SortSelector onSelectSortOrder={onSelectSortOrder} selectSortOrder={selectSortOrder} />
